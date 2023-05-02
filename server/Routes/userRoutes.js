6e51576d058a95c8415618e1084e0103269e9af0
Saumya <usam9010@gmail.com>
2023-05-02 23:08:37 +0530

    checking

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userController = require('../Controllers/userController');
 const authController = require('../Controllers/authController');
 
-const router = express.Router();
+const router = express.Router();  
 
 router.post('/signup', authController.signup);
 // router.post('/login', authController.login);
