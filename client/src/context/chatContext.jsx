04ab5b36091227ff92eff4ac1d04b5be734d7680
Saumya <usam9010@gmail.com>
2023-05-13 23:54:40 +0530

    working on creating chats

--- client/src/context/chatContext.jsx
+++ client/src/context/chatContext.jsx
@@ -7,9 +7,35 @@ export const ChatContextProvider = ({ children, user }) => {
   const [userChats, setUserChats] = useState(null);
   const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
   const [userChatsError, setUserChatsError] = useState(null);
+  const [potentialChats , setPotentialChats] = useState(null);
+useEffect(()=>{
+  const getUsers = async()=>{
+    const response = await getRequest(`${baseUrl}/users`);
+
+ 
+    if (response.error) {
+      return console.log("error fetching users",response);
+    }
+
+    const pChats = response.filter((u)=>{
+      let isChatCreated = false;
+
+      if(user.data.user._id === u._id) return false;
+
+      if(userChats){
+        userChats?.some((chat)=>{
+          return chat.members[0] === u._id || chat.members[1] === u._id
+        })
+      }
+
+    })
+
+  };
+  getUsers();
+
+})
 
 
-  console.log('user',user?.data?.user?._id);
   useEffect(() => {
     const getUserChats = async () => {
 
@@ -17,7 +43,7 @@ export const ChatContextProvider = ({ children, user }) => {
         setIsUserChatsLoading(true);
         setUserChatsError(null);
 
-        const response = await getRequest(`${baseUrl}/chats/${user?.data?.user?._id}`);
+        const response = await getRequest(`${baseUrl}/getUser/${user?.data?.user?._id}`);
 
         setIsUserChatsLoading(false);
 
